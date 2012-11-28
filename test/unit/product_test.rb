4b819111b2030ab943474817e24038a7057370b6
Aldo Utrera <Kugel85@gmail.com>
2012-11-28 12:12:33 -0600

    Adding the model unit tests, product fixtures and fixing typo in product model validations

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+
+  fixtures :products
+
   test "product attributes should not be empty" do
     product = Product.new
     assert product.invalid?
@@ -16,16 +19,47 @@ class ProductTest < ActiveSupport::TestCase
       description: "yyy",
       image_url: "zzz.jpg"
     )
-    product.price = -1
+    product.price = -1.00
     assert product.invalid?
-    assert_equal "must be greater than or equal to 0.01",
-    product.errors[:price].join('; ')
-    product.price = 0
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+    product.price = 0.00
     assert product.invalid?
-    assert_equal "must be greater than or equal to 0.01",
-    product.errors[:price].join('; ')
-    product.price = 1
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+    product.price = 1.00
     assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "My book title",
+                description: "yyy",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.jpg FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join('; ')
+
   end
 
 end
