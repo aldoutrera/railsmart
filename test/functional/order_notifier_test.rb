36845953acceea4c93c4e22ef8d821716b020f74
Aldo Utrera <Kugel85@gmail.com>
2012-12-03 16:24:31 -0600

    Adding the functional test of the order_notifier

--- test/functional/order_notifier_test.rb
+++ test/functional/order_notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Order confirmation", mail.subject
+    assert_equal ["something@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "/1 x Programming Ruby 1.9/", mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Order shipped!", mail.subject
+    assert_equal ["something@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "/Programming Ruby 1.9/", mail.body.encoded
   end
 
 end
