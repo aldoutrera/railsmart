6a7cce6f0bcb53ead0d801deeb5cb7791da29d7c
Aldo Utrera <Kugel85@gmail.com>
2012-12-18 11:13:59 -0600

    Setting up the locale on a filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_i18n_locale_from_params
   before_filter :authorize
   protect_from_forgery
 
   protected
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 
     def authorize
       unless User.find_by_id(session[:user_id])
@@ -11,7 +26,6 @@ class ApplicationController < ActionController::Base
     end
 
   private
-
     def current_cart
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
